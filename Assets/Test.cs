f3c42763a7d1e47adf62628f061432633fc37c13
STD\s20227017 <20227017-kitagawatoshiaki@hcs.ac.jp>
2024-06-07 09:18:20 +0900

    s20227017-2

---
+++ Assets/Test.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    private float[] frames = new float[10];
+    private string[] _input = new string[10];
+    private int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {   
+
+    }
+}
